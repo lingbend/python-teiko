4bf5a4ab7439059a3c9821b6fa350a3a7ec59337
lingbend <lingbend@gmail.com>
2025-03-22 22:53:18 -0600

    Simplified code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 
 INPUT_FILE_NAME = "cell-count.csv"
 ENCODING_TYPE = "utf-8-sig"
+CELL_TYPES = ["b_cell", "cd8_t_cell", "cd4_t_cell", "nk_cell", "monocyte"]
 
 
 class dataLine:
@@ -42,6 +43,12 @@ def getPercentages(data, keepKeys, addKeys, percentKeys):
             line._data[addKeys[3]] = line._data[addKeys[2]] / total
         percentData.extend(sampleData)
     return newKeys, percentData
+
+def getTotal(dataLine):
+    total = 0
+    for key in CELL_TYPES:
+        total += int(dataLine._data[key])
+    return total
 
 def filterByKeyPairs(data, pairs):
     filteredData = []
@@ -90,19 +97,31 @@ def getFile(fileName = INPUT_FILE_NAME):
 def closeFile(file):
     file.close()
 
+def getBoxPlotData(fields, data, processing=(lambda x, field : x)):
+    dataSet = []
+    for field in fields:
+        dataSet.append([processing(point, field) for point in data])
+    return dataSet
+
 if __name__ == "__main__":
     dataFile = getFile()
     keys = getKeys(dataFile.readline())
     data = getData(dataFile, keys)
-    newKeys, newData = getPercentages(data, ["sample"], ["total_count", "population", "count", "percentage"],
-                                      ["b_cell", "cd8_t_cell", "cd4_t_cell", "nk_cell", "monocyte"])
+    newKeys, newData = getPercentages(data, ["sample"], ["total_count", "population", "count", "percentage"], CELL_TYPES)
     packToCSV(newData, newKeys)
 
     tr1Data = filterByKeyPairs(data, {"condition":"melanoma", "treatment":"tr1", "sample_type":"PBMC"})
     tr1ResponderData = filterByKeyPairs(tr1Data, {"response":"y"})
     tr1NonResponderData = filterByKeyPairs(tr1Data, {"response":"n"})
-    
-    # print(len(data))
-    # for i in data:
-    #     print(i)
+
+    processedResponderData = getBoxPlotData(CELL_TYPES, tr1ResponderData, (lambda point, field: int(point._data.get(field)) / getTotal(point)))
+    processedNonResponderData = getBoxPlotData(CELL_TYPES, tr1NonResponderData, (lambda point, field: int(point._data.get(field)) / getTotal(point)))
+
+    plotArrays = [None] * len(processedNonResponderData * 2)
+    plotArrays[::2] = processedResponderData
+    plotArrays[1::2] = processedNonResponderData
+
+    mat.boxplot(plotArrays, tick_labels=["R b", "Non-R b", "R cd8 t", "Non-R cd8 t", "R cd4 t", "Non-R cd4 t", "R nk", "Non-R nk", "R monocyte", "Non-R monocyte"])
+    mat.show()
+
     closeFile(dataFile)
